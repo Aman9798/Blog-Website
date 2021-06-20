3fd56847983827dc3099f74c5dae0d8e464d3394
amank <amaniitg2001@gmail.com>
2021-06-21 00:53:01 +0530

    logout fix

--- Blog/Blog/urls.py
+++ Blog/Blog/urls.py
@@ -4,13 +4,14 @@ from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns   # for serving static files like images
 from django.conf.urls.static import static
 from django.conf import settings
+from articles import views as article_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('articles/', include('articles.urls')),
     path('about/',views.About),
-    path('',views.Homepage),   #home page
+    path('',article_views.Articles_List, name='home'),   #home page # redirects to articles list
 ]
 
 urlpatterns += staticfiles_urlpatterns()
