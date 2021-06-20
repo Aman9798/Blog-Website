4a97af3ccc13d388b6e17cd09cffa89090a44f25
amank <amaniitg2001@gmail.com>
2021-06-20 23:27:10 +0530

    attched user with blog

--- Blog/articles/models.py
+++ Blog/articles/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import SET_DEFAULT
 
 # Create your models here.
 class Article(models.Model):
@@ -7,6 +9,7 @@ class Article(models.Model):
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     thumb = models.ImageField(default='default.png', blank=True)
+    author = models.ForeignKey(User, default=None, on_delete=SET_DEFAULT)
 
     def __str__(self):
         return self.title         # model objects are now shown with their title in admin section and shell
--- Blog/articles/views.py
+++ Blog/articles/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Article
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from . import forms
 # Create your views here.
 def Articles_List(request):
     article_objects = Article.objects.all().order_by('date')
@@ -14,5 +15,15 @@ def Article_detail(request, slug1):
 
 @login_required(login_url="/accounts/login/")
 def Article_create(request):
-    return render (request, 'articles/ArticleCreate.html')
+    if request.method == 'POST':
+        form = forms.CreateArticle(request.POST, request.FILES)
+        if form.is_valid():
+            # save article
+            instance1 = form.save(commit=False)  # returns the instance just before saving the form
+            instance1.author = request.user         # attachs the user with form
+            instance1.save()     # finally saves it
+            return redirect('articles:list')
+    else:
+        form = forms.CreateArticle()
+    return render (request, 'articles/ArticleCreate.html', {'form': form})
    # return HttpResponse("ngnik ")
